2e48419e2bd2505915041f6e59024c57aab72a1d
Imanol Celaya <ilcra1989@gmail.com>
2014-10-29 02:34:28 +0100

    added some debugging code, mgiht need to setup every address mapping for it to work, although it return EFI_SUCCESS

--- bootloader/vmmem.c
+++ bootloader/vmmem.c
@@ -13,11 +13,16 @@ EFI_STATUS EFIAPI SetVM(IN UINTN size, IN UINTN descriptorsize, IN UINTN descrip
 
   int * towrite = (int*)tmp->PhysicalStart;
 
-  (*(EFI_PHYSICAL_ADDRESS*)0x150) = (EFI_PHYSICAL_ADDRESS)towrite;
-  (*towrite) = (int)0xABCD;
-  // we found the location
-  tmp->VirtualStart = 0x40000000;
+  int * toVwrite = (int*)tmp->VirtualStart;
 
+  (*(EFI_PHYSICAL_ADDRESS*)0x150) = (EFI_PHYSICAL_ADDRESS)towrite;
+  (*(EFI_PHYSICAL_ADDRESS*)0x200) = (EFI_PHYSICAL_ADDRESS)toVwrite;
+  (*towrite) = (int)0xABCD;
+  (*toVwrite) = (int)0xDCBA;
+  // we found the location
+  tmp->VirtualStart = 0x40000000; // For Some Reason it doesn't get applied, might need to setup every mapping also?
+  (*(EFI_PHYSICAL_ADDRESS*)0x200) = size;
+  (*(EFI_PHYSICAL_ADDRESS*)0x150) = descriptorsize;
   return uefi_call_wrapper(RT->SetVirtualAddressMap, 4, size, descriptorsize, descriptorversion, map);
 }
 
