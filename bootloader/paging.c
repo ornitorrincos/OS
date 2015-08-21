a25e9fca5f1b99831d4553b4cd445ffda6876fd3
ornitorrincos <ilcra1989@gmail.com>
2015-08-21 22:05:35 +0200

    linked the kernel to -2GB vaddr

--- bootloader/paging.c
+++ bootloader/paging.c
@@ -23,12 +23,23 @@ void * SetPagingStructs()
   // set base CR3 value
   // start setting paging at the next 4kb aligment
   void * base_address = (void*)pages;
+  void * next_address = (void*)(((unsigned char*)pages) + 4*1024);
 
   ((s_CR3*)base_address)->PWT = 0;
   ((s_CR3*)base_address)->PCD = 0;
-  ((s_CR3*)base_address)->base_addr = (uchar)((unsigned char*)base_address + 1024*4);
+  ((s_CR3*)base_address)->base_addr = (uchar)(next_address); // PML4E table
 
-  base_address = (void*)(((s_CR3*)base_address)->base_addr);
+  //base_address = (void*)(((s_CR3*)base_address)->base_addr);
+  base_address = next_address;
+  next_address = (void*)(((unsigned char*)base_address) + 4*1024);
+
+  // PML4E
+
+  // PDPTE
+
+  // PTE
+
+
 
 
   return (void*)pages;
